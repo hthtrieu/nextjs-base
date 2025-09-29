9296b1880bee9d64fc17b12b6fda4330a8705d18
hthtrieu <hthtrieu1002@gmail.com>
2025-09-29 23:08:41 +0700

    add: course image

--- src/components/home/promotions/PromotionCard.tsx
+++ src/components/home/promotions/PromotionCard.tsx
@@ -2,14 +2,14 @@ import { Card, CardContent } from "@/components/ui/card";
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 
-export const PromotionCard = ({ facility }: { facility: any }) => {
+export const PromotionCard = ({ promotion }: { promotion: any }) => {
   return (
     <div>
       <Card className="group p-0 overflow-hidden rounded-none">
-        <CardContent className="flex aspect-16/9 max-h-[480px] items-center justify-center p-0 relative">
+        <CardContent className="flex aspect-2/1 max-h-[480px] items-center justify-center p-0 relative">
           <img
-            src={facility.image}
-            alt={facility.name}
+            src={promotion.image}
+            alt={promotion.name}
             className="h-full w-full object-cover"
           />
         </CardContent>
